23e2188413fb9102b4114c04e338e568241b46cc
Alex Costa <alexs.alexsandrocosta@hotmail.com>
2021-08-04 15:23:07 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/advocacia/cursomc/CursomcApplication.java
+++ src/main/java/com/advocacia/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.advocacia.cursomc.domain.Cidade;
 import com.advocacia.cursomc.domain.Cliente;
 import com.advocacia.cursomc.domain.Endereco;
 import com.advocacia.cursomc.domain.Estado;
+import com.advocacia.cursomc.domain.ItemPedido;
 import com.advocacia.cursomc.domain.Pagamento;
 import com.advocacia.cursomc.domain.PagamentoComBoleto;
 import com.advocacia.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.advocacia.cursomc.repositories.CidadeRepository;
 import com.advocacia.cursomc.repositories.ClienteRepository;
 import com.advocacia.cursomc.repositories.EnderecoRepository;
 import com.advocacia.cursomc.repositories.EstadoRepository;
+import com.advocacia.cursomc.repositories.ItemPedidoRepository;
 import com.advocacia.cursomc.repositories.PagamentoRepository;
 import com.advocacia.cursomc.repositories.PedidoRepository;
 import com.advocacia.cursomc.repositories.ProdutoRepository;
@@ -47,6 +49,9 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -112,6 +117,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 
 	}
 
--- src/main/java/com/advocacia/cursomc/domain/Produto.java
+++ src/main/java/com/advocacia/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.advocacia.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -34,7 +37,8 @@ public class Produto implements Serializable{
 	
 	private List<Categoria> categorias = new ArrayList<>();
 	
-	
+	@OneToMany(mappedBy="id.produto")	
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Produto() {
 		
@@ -45,6 +49,14 @@ public class Produto implements Serializable{
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 	
 	public Integer getId() {
@@ -78,6 +90,15 @@ public class Produto implements Serializable{
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -86,6 +107,7 @@ public class Produto implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -103,6 +125,7 @@ public class Produto implements Serializable{
 			return false;
 		return true;
 	}
+
 	
 	
 	
